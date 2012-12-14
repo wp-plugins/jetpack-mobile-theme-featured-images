15b5ada89d085642cf957c4cd88b493ed62707b6
jeherve <jeherve@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-12-14 09:47:27 +0000

    Make a few coding standard/style cleanups
    git-svn-id: https://plugins.svn.wordpress.org/jetpack-mobile-theme-featured-images/trunk@639086 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- jp-minileven-featured.php
+++ jp-minileven-featured.php
@@ -4,7 +4,7 @@
  * Plugin URI: http://wordpress.org/extend/plugins/jetpack-mobile-theme-featured-images/
  * Description: Adds Featured Images before the content on the home page, in Jetpack Mobile theme
  * Author: Jeremy Herve
- * Version: 1.1
+ * Version: 1.2
  * Author URI: http://jeremyherve.com
  * License: GPL2+
  * Text Domain: jetpack
@@ -13,25 +13,23 @@
 // Check if we are on mobile
 // Props @saracannon http://ran.ge/2012/12/05/parallax-and-mobile/
 function tweakjp_is_mobile_or_tablet() {
-    if ( class_exists( 'Jetpack_User_Agent_Info' ) ) {
-        $ua_info = new Jetpack_User_Agent_Info();
-        return ( jetpack_is_mobile() || $ua_info->is_tablet() );
-    }
-    return false;
+    if ( ! class_exists( 'Jetpack_User_Agent_Info' ) )
+    	return false;
+
+    $ua_info = new Jetpack_User_Agent_Info();
+    return ( jetpack_is_mobile() || $ua_info->is_tablet() );
 }
 
 // On Mobile? Let's add the Featured Image
-if ( tweakjp_is_mobile_or_tablet() ) {
-	add_filter( 'the_title', 'tweakjp_minileven_featuredimage' );
+function tweakjp_maybe_add_filter() {
+	if ( tweakjp_is_mobile_or_tablet() )
+		add_filter( 'the_title', 'tweakjp_minileven_featuredimage' );
 }
-
+add_action( 'wp_head', 'tweakjp_maybe_add_filter' );
 
 function tweakjp_minileven_featuredimage( $title ) {
-	if ( has_post_thumbnail() && is_home() && in_the_loop() ) {
-	    $featured_content = get_the_post_thumbnail();
-	    $title .= $featured_content;
-	    return $title;
-	} else {
-		return $title;
-	}
+	$tweak = has_post_thumbnail() && is_home() && in_the_loop();
+	$featured_content = ( $tweak ) ? get_the_post_thumbnail() : '';
+
+	return $title . $featured_content;
 }
